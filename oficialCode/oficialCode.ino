ab8ab07046ab3dc801c23b8266f79354abd9da57
Felipe K. Lisak <felipeklisak@gmail.com>
2024-06-12 18:09:23 -0300

    corrects 90 degres curves in  ´oficialCode.ino´ archive

--- oficialCode/oficialCode.ino
+++ oficialCode/oficialCode.ino
@@ -14,7 +14,7 @@ void setup() {
 
 void loop() {
   // Verificating if infrareds are working together
-  if ((((digitalRead(8)) && (digitalRead(9))) == (1))) {
+  if (((digitalRead(8)) && (digitalRead(9))) == (1)) {
     //Turning each Engine On
     analogWrite(3, 0);
     analogWrite(4, 85);
@@ -23,21 +23,39 @@ void loop() {
     delay(200);
   }
   //Direction ajust by engine
-  if ((digitalRead(7)) == (digitalRead(8)) > (1)) {
+  if ((digitalRead(7)) < (1) && (digitalRead(10)) == (1) && (digitalRead(8)) < (1) && (digitalRead(9)) < (1)) {
     analogWrite(3, 0);
     analogWrite(4, 0);
-    delay(100);
-    analogWrite(5, 0);
-    analogWrite(6, 100);
-    delay(100);
-  }
-  if ((digitalRead(9)) == (digitalRead(10)) > (1)) {
-    analogWrite(3, 0);
-    analogWrite(4, 100);
-    delay(100);
     analogWrite(5, 0);
     analogWrite(6, 0);
-    delay(100);
+    delay(50);
+    analogWrite(3, 0);
+    analogWrite(4, 60);
+    analogWrite(5, 50);
+    analogWrite(6, 0);
+    delay(50);
+    analogWrite(3, 0);
+    analogWrite(4, 85);
+    analogWrite(5, 0);
+    analogWrite(6, 0);
+    delay(600);
+  }
+  if ((digitalRead(10)) < (1) && (digitalRead(7)) == (1) && (digitalRead(8)) < (1) && (digitalRead(9)) < (1)) {
+    analogWrite(3, 0);
+    analogWrite(4, 0);
+    analogWrite(5, 0);
+    analogWrite(6, 0);
+    delay(50);
+    analogWrite(3, 60);
+    analogWrite(4, 0);
+    analogWrite(5, 0);
+    analogWrite(6, 50);
+    delay(50);
+    analogWrite(3, 0);
+    analogWrite(4, 0);
+    analogWrite(5, 0);
+    analogWrite(6, 75);
+    delay(600);
   }
   //Align ajust by engine
   if ((digitalRead(8)) < (1)) {
