f03d00d3fe1fd76dd16a1e4fafde7f6b767809ea
Felipe K. Lisak <felipeklisak@gmail.com>
2024-06-13 13:29:19 -0300

    Update oficialCode.ino

--- oficialCode/oficialCode.ino
+++ oficialCode/oficialCode.ino
@@ -1,15 +1,15 @@
 void setup() {
   // Engine
-  pinMode(3, OUTPUT); //Left Engine
-  pinMode(4, OUTPUT); //Left Engine
-  pinMode(5, OUTPUT); //Right Engine
-  pinMode(6, OUTPUT); //Right Engine
+  pinMode(3, OUTPUT);  //Left Engine
+  pinMode(4, OUTPUT);  //Left Engine
+  pinMode(5, OUTPUT);  //Right Engine
+  pinMode(6, OUTPUT);  //Right Engine
   // Line Follower Sensor
-  pinMode(8, INPUT); //Right
-  pinMode(9, INPUT); //Left
+  pinMode(8, INPUT);  //Right
+  pinMode(9, INPUT);  //Left
   // 90 degres curve Sensor
-  pinMode(7, INPUT); //Right
-  pinMode(10, INPUT); //Left
+  pinMode(7, INPUT);   //Right
+  pinMode(10, INPUT);  //Left
 }
 
 void loop() {
@@ -23,53 +23,56 @@ void loop() {
     delay(200);
   }
   //Direction ajust by engine
-  if ((digitalRead(7)) < (1) && (digitalRead(10)) == (1) && (digitalRead(8)) < (1) && (digitalRead(9)) < (1)) {
-    analogWrite(3, 0);
-    analogWrite(4, 0);
-    analogWrite(5, 0);
-    analogWrite(6, 0);
-    delay(50);
-    analogWrite(3, 0);
-    analogWrite(4, 60);
-    analogWrite(5, 50);
-    analogWrite(6, 0);
-    delay(50);
-    analogWrite(3, 0);
-    analogWrite(4, 85);
-    analogWrite(5, 0);
-    analogWrite(6, 0);
-    delay(600);
-  }
-  if ((digitalRead(10)) < (1) && (digitalRead(7)) == (1) && (digitalRead(8)) < (1) && (digitalRead(9)) < (1)) {
-    analogWrite(3, 0);
-    analogWrite(4, 0);
-    analogWrite(5, 0);
-    analogWrite(6, 0);
-    delay(50);
-    analogWrite(3, 60);
-    analogWrite(4, 0);
-    analogWrite(5, 0);
-    analogWrite(6, 50);
-    delay(50);
-    analogWrite(3, 0);
-    analogWrite(4, 0);
-    analogWrite(5, 0);
-    analogWrite(6, 75);
-    delay(600);
-  }
-  //Align ajust by engine
-  if ((digitalRead(8)) < (1)) {
-    analogWrite(3, 0);
-    analogWrite(4, 75);
+  if ((digitalRead(7)) != (digitalRead(10))) {
+    if ((digitalRead(7)) < (1) && (digitalRead(10)) == (1) && (digitalRead(8)) < (1) && (digitalRead(9)) < (1)) {
+      analogWrite(3, 0);
+      analogWrite(4, 0);
+      analogWrite(5, 0);
+      analogWrite(6, 0);
+      delay(50);
+      analogWrite(3, 0);
+      analogWrite(4, 60);
+      analogWrite(5, 50);
+      analogWrite(6, 0);
+      delay(50);
+      analogWrite(3, 0);
+      analogWrite(4, 85);
+      analogWrite(5, 0);
+      analogWrite(6, 0);
+      delay(600);
+    }
+    if ((digitalRead(10)) < (1) && (digitalRead(7)) == (1) && (digitalRead(8)) < (1) && (digitalRead(9)) < (1)) {
+      analogWrite(3, 0);
+      analogWrite(4, 0);
+      analogWrite(5, 0);
+      analogWrite(6, 0);
+      delay(50);
+      analogWrite(3, 60);
+      analogWrite(4, 0);
+      analogWrite(5, 0);
+      analogWrite(6, 50);
+      delay(50);
+      analogWrite(3, 0);
+      analogWrite(4, 0);
+      analogWrite(5, 0);
+      analogWrite(6, 75);
+      delay(600);
+    }
   } else {
-    analogWrite(3, 0);
-    analogWrite(4, 0);
-  }
-  if ((digitalRead(9)) < (1)) {
-    analogWrite(5, 0);
-    analogWrite(6, 75);
-  } else {
-    analogWrite(5, 0);
-    analogWrite(6, 0);
+    //Align ajust by engine
+    if ((digitalRead(8)) < (1)) {
+      analogWrite(3, 0);
+      analogWrite(4, 75);
+    } else {
+      analogWrite(3, 0);
+      analogWrite(4, 0);
+    }
+    if ((digitalRead(9)) < (1)) {
+      analogWrite(5, 0);
+      analogWrite(6, 75);
+    } else {
+      analogWrite(5, 0);
+      analogWrite(6, 0);
+    }
   }
 }
